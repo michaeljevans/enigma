14a0f437b7d1c614239ea681a5958a0c719032b1
Michael Evans <michaeljevans113@gmail.com>
2020-06-06 14:17:11 -0600

    Add #setup

--- test/date_generator_test.rb
+++ test/date_generator_test.rb
@@ -5,16 +5,16 @@ require 'minitest/pride'
 require './lib/date_generator'
 
 class DateGeneratorTest < MiniTest::Test
-  def test_it_exists
-    date_generator = DateGenerator.new
+  def setup
+    @date_generator = DateGenerator.new
+  end
 
-    assert_instance_of DateGenerator, date_generator
+  def test_it_exists
+    assert_instance_of DateGenerator, @date_generator
   end
 
   def test_it_generates_todays_date
-    # Tested on 2020/06/04
-    date_generator = DateGenerator.new
-
-    assert_equal '040620', date_generator.generate_date
+    # Tested on 2020/06/06
+    assert_equal '060620', @date_generator.generate_date
   end
 end

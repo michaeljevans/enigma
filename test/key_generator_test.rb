e6b90c3336be1c8006a263b849ddd449b49f58e1
Michael Evans <michaeljevans113@gmail.com>
2020-06-06 14:15:46 -0600

    Add #setup

--- test/key_generator_test.rb
+++ test/key_generator_test.rb
@@ -6,16 +6,16 @@ require 'mocha/minitest'
 require './lib/key_generator'
 
 class KeyGeneratorTest < MiniTest::Test
-  def test_it_exists
-    keygen = KeyGenerator.new
-
-    assert_instance_of KeyGenerator, keygen
+  def setup
+    @keygen = KeyGenerator.new
   end
 
-  def test_it_generates_random_number
-    keygen = KeyGenerator.new
+  def test_it_exists
+    assert_instance_of KeyGenerator, @keygen
+  end
 
-    keygen.expects(:rand).returns('00357')
-    assert_equal '00357', keygen.generate_key
+  def test_it_generates_random_five_digit_number
+    @keygen.expects(:rand).returns('00357')
+    assert_equal '00357', @keygen.generate_key
   end
 end

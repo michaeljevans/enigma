fb50cd63a74c40d475200c4c50ccd570bd7c0815
Michael Evans <michaeljevans113@gmail.com>
2020-06-07 16:09:50 -0600

    Add #initialize

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,13 +1,12 @@
+require_relative 'shift_generator'
+require_relative 'key_generator'
+require_relative 'cryptographer'
+require 'date'
+
 class Enigma
+  attr_reader :today
   def initialize
-
-  end
-
-  def encrypt(message, key, date)
-
-  end
-
-  def decrypt(ciphertext, key, date)
-
+    @today  = Date.today.strftime('%d%m%y')
+    @crypto = Cryptographer.new
   end
 end

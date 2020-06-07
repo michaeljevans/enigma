cf0f16d0a8c123558f83b7e9afd01edd972bd16a
Michael Evans <michaeljevans113@gmail.com>
2020-06-07 14:44:36 -0600

    Add attribute test

--- lib/cryptographer.rb
+++ lib/cryptographer.rb
@@ -1,3 +1,6 @@
 class Cryptographer
-
+  attr_reader :alphabet
+  def initialize
+    @alphabet = ("a".."z").to_a << " "
+  end
 end
--- test/cryptographer_test.rb
+++ test/cryptographer_test.rb
@@ -1,3 +1,5 @@
+require 'simplecov'
+SimpleCov.start
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/cryptographer'
@@ -5,9 +7,14 @@ require './lib/cryptographer'
 class CryptographerTest < Minitest::Test
   def setup
     @crypto = Cryptographer.new
+    @shifts = [3, 27, 73, 20]
   end
 
   def test_it_exists
     assert_instance_of Cryptographer, @crypto
+  end
+
+  def test_it_has_readable_attributes
+    assert_equal ("a".."z").to_a << " ", @crypto.alphabet
   end
 end

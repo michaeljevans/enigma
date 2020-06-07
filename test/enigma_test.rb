5d1fefb9c0e9fef11912396062ab1fb67c66b907
Michael Evans <michaeljevans113@gmail.com>
2020-06-07 15:53:32 -0600

    Add attribute test

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -12,4 +12,9 @@ class EnigmaTest < Minitest::Test
   def test_it_exists
     assert_instance_of Enigma, @enigma
   end
+
+  def test_it_has_readable_attributes
+    # Tested on 2020-06-07
+    assert_equal '070620', @enigma.today
+  end
 end

a6f5b27625b280d40b6b965fff240236ba802351
Michael Evans <michaeljevans113@gmail.com>
2020-06-08 15:44:17 -0600

    Add CLI functionality for decrypting messages

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,3 +1,8 @@
 require_relative 'enigma'
 
 enigma = Enigma.new
+
+encrypted = IO.read(ARGV[0]).chomp
+decrypted = enigma.decrypt(encrypted, ARGV[2], ARGV[3])
+IO.write(ARGV[1], decrypted[:decryption])
+p "Created '#{ARGV[1]}' with key #{decrypted[:key]} and date #{decrypted[:date]}"

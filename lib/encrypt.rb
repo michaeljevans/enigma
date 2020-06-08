107b2aea9708f7ba2293707fb382cfde880a877f
Michael Evans <michaeljevans113@gmail.com>
2020-06-08 15:41:09 -0600

    Remove logging to crypt_log.txt

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,9 +1,8 @@
-require 'date'
 require_relative 'enigma'
 
 enigma = Enigma.new
-message = IO.read(ARGV[0]).chomp
+
+message   = IO.read(ARGV[0]).chomp
 encrypted = enigma.encrypt(message)
 IO.write(ARGV[1], encrypted[:encryption])
-IO.write('crypt_log.txt', "Encrypted '#{message}' with key #{encrypted[:key]} on #{Date.today.strftime('%Y-%m-%d')}\n", mode: 'a')
 p "Created '#{ARGV[1]}' with key #{encrypted[:key]} and date #{encrypted[:date]}"

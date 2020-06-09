0a5c915ea2d87c8680506732af12e7ab2d4f7c03
Michael Evans <michaeljevans113@gmail.com>
2020-06-08 21:41:30 -0600

    Add functionality for #crack method

--- modules/fileable.rb
+++ modules/fileable.rb
@@ -5,7 +5,9 @@ module Fileable
     enigma = Enigma.new
     altered = enigma.encrypt(IO.read(ARGV[0]).chomp) if action == :encrypt
     altered = enigma.decrypt(IO.read(ARGV[0]).chomp, ARGV[2], ARGV[3]) if action == :decrypt
+    altered = enigma.crack(IO.read(ARGV[0]).chomp, ARGV[2]) if action == :crack
     IO.write(ARGV[1], altered.values.first)
-    p "Created '#{ARGV[1]}' with key #{altered[:key]} and date #{altered[:date]}"
+    p "Created '#{ARGV[1]}' with key #{altered[:key]} and date #{altered[:date]}" if action == :encrypt || action == :decrypt
+    p "Created '#{ARGV[1]}' with cracked key #{altered[:key]} and date #{altered[:date]}" if action == :crack
   end
 end
